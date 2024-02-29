800c8d3ad529639b2f3b54014245c23319f66925
SergeyK1983 <ssp-serg@yandex.ru>
2024-02-29 15:38:50 +0300

    models_complect

--- backend/app_task/admin.py
+++ backend/app_task/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import User, Product, StudentsToProduct, Lesson, Group, StudentsInGroup
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'teacher', 'student')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'name', 'price', 'start', 'min_quantity', 'max_quantity')
+
+
+class StudentsToProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'product')
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_id', 'name', 'url_video')
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_id', 'name', 'min_quantity', 'max_quantity')
+
+
+class StudentsInGroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'group')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(StudentsToProduct, StudentsToProductAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(StudentsInGroup, StudentsInGroupAdmin)
